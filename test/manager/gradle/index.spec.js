f8f9a5af94fedf8a2c1bd43cf95f24cbdbcac47b
corecanarias <israel_bethencourt@intuit.com>
2018-10-17 11:35:59 +0100

    Improve getPackageUpdates function

--- test/manager/gradle/index.spec.js
+++ test/manager/gradle/index.spec.js
@@ -14,32 +14,6 @@ const config = {
   },
 };
 
-// prettier-ignore
-const depedenciesFromBuildGradleExample1 = {
-  deps: [{
-      currentValue: '0.1',
-      depName: 'com.fkorotkov:gradle-libraries-plugin',
-    }, {
-      currentValue: '0.2.3',
-      depName: 'gradle.plugin.se.patrikerdes:gradle-use-latest-versions-plugin',
-    }, {
-      currentValue: '1.3',
-      depName: 'org.hamcrest:hamcrest-core',
-    }, {
-      currentValue: '3.1',
-      depName: 'cglib:cglib-nodep',
-    }, {
-      currentValue: '6.0.9.RELEASE',
-      depName: 'org.grails:gorm-hibernate5-spring-boot',
-    }, {
-      currentValue: '5.1.41',
-      depName: 'mysql:mysql-connector-java',
-    }, {
-      currentValue: '1.5.2.RELEASE',
-      depName: 'org.springframework.boot:spring-boot-starter-test',
-  }],
-};
-
 const updatesDependenciesReport = fsReal.readFileSync(
   'test/_fixtures/gradle/updatesReport.json',
   'utf8'
@@ -60,7 +34,7 @@ describe('manager/gradle', () => {
         config
       );
 
-      expect(dependencies).toEqual(depedenciesFromBuildGradleExample1);
+      expect(dependencies).toMatchSnapshot();
     });
 
     it('should return null if there is no dependencies', async () => {
@@ -98,7 +72,10 @@ describe('manager/gradle', () => {
       expect(exec.mock.calls[0][0]).toBe(
         'gradle --init-script init.gradle dependencyUpdates -Drevision=release'
       );
-      expect(exec.mock.calls[0][1]).toMatchSnapshot();
+      expect(exec.mock.calls[0][1]).toMatchObject({
+        "cwd": "localDir",
+        "timeout": 20000,
+      });
     });
 
     it('should write the gradle config file in the tmp dir', async () => {
@@ -116,34 +93,30 @@ describe('manager/gradle', () => {
   });
 
   describe('getPackageUpdates', () => {
-    it('should return outdated dependencies', async () => {
-      // prettier-ignore
-      const expectedOutdatedDependencies = [{
-          depGroup: "cglib", name: "cglib-nodep", version: "3.1",
-          available: {release: "3.2.8", milestone: null, integration: null},
-        }, {
-          depGroup: "org.grails", name: "gorm-hibernate5-spring-boot", version: "6.0.9.RELEASE",
-          available: {release: "6.1.10.RELEASE", milestone: null, integration: null}
-        }, {
-          depGroup: "mysql", name: "mysql-connector-java", version: "5.1.41",
-          available: {release: "8.0.12", milestone: null, integration: null}
-        }, {
-          depGroup: "org.springframework.boot", name: "spring-boot-starter-test", version: "1.5.2.RELEASE",
-          available: {release: "2.0.5.RELEASE", milestone: null, integration: null}
-      }];
+    it('should return the new version if it is available', async () => {
+      const newVersion = {
+        ...config,
+        depName: "cglib:cglib-nodep",
+        available: {
+          release: "3.2.8"
+        }
+      };
+      const outdatedDependencies = await manager.getPackageUpdates(newVersion);
 
-      const outdatedDependencies = await manager.getPackageUpdates(config);
-
-      expect(outdatedDependencies).toMatchObject(expectedOutdatedDependencies);
+      expect(outdatedDependencies).toMatchObject([{
+        depName: 'cglib:cglib-nodep',
+        newValue: "3.2.8"
+      }]);
     });
 
-    it('should read the right updates report', async () => {
-      await manager.getPackageUpdates(config);
+    it('should return empty if there is no new version', async () => {
+      const newVersion = {
+        ...config,
+        depName: "cglib:cglib-nodep",
+      };
+      const outdatedDependencies = await manager.getPackageUpdates(newVersion);
 
-      expect(fs.readFile.mock.calls.length).toBe(1);
-      expect(fs.readFile.mock.calls[0][0]).toBe(
-        'localDir/build/dependencyUpdates/report.json'
-      );
+      expect(outdatedDependencies).toMatchObject([]);
     });
   });
 
